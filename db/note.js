f7313707690a4a6c48e9af4c49b1aa55f02354e3
Champion113 <sbalbright19@gmail.com>
2020-10-18 18:00:43 -0400

    corrected errors

--- db/note.js
+++ db/note.js
@@ -1,21 +1,19 @@
 const db = require('./db.json')
 const fs = require('fs');
 const util = require("util");
-​
 const { v4: uuidv4 } = require("uuid")
-​
 const newReadFile = util.promisify(fs.readFile)
 const newWriteFile = util.promisify(fs.writeFile)
-​
+
 class Notes {
-​
+
     read() {
         return newReadFile("db/db.json", "utf-8")
     }
     write(note) {
         return newWriteFile("db/db.json", JSON.stringify(note))
     }
-​
+
     getAllNotes() {
         return this.read().then(function (notes) {
             var notesArray = [];
@@ -24,26 +22,26 @@ class Notes {
             } catch (error) {
                 notesArray = [];
             }
-​
+
             return notesArray;
         })
     }
-​
+
     addNote(note) {
-​
+
         const { title, text } = note;
         const newNote = {
             title,
             text,
             id: uuidv4()
         }
-​
+
         return this.getAllNotes().then((notesArray) => [...notesArray, newNote]).then(newNote => this.write(newNote))
-​
+
     }
     delete(id) {
         return this.getAllNotes().then(notesArray => notesArray.filter(note => note.id !== id)).then(filteredArray => this.write(filteredArray))
     }
 }
-​
+
 module.exports = new Notes();
